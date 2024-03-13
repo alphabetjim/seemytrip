50f53e04ba5f285b3c0f7f7fbf8195519b59f239
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-13 20:53:02 +0000

    Add update functionality for Traveller model.

--- travellerprofile/forms.py
+++ travellerprofile/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Traveller
 
 
-class TravellerForm(forms.Form):
+class TravellerForm(forms.ModelForm):
     """
     Form class for users to create their Traveller profile
     """
@@ -10,3 +10,7 @@ class TravellerForm(forms.Form):
     lastname = forms.CharField(max_length=50)
     bio = forms.CharField(widget=forms.Textarea(attrs={'type': 'text','rows': 4, 'cols': 40}))
     profile_photo = forms.ImageField()
+
+    class Meta:
+        model = Traveller
+        fields = ('firstname', 'lastname', 'bio', 'profile_photo')
