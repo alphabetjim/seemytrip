78b2fb6083f8347fbf28b9aa35b34afe0e62d45f
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-13 17:01:55 +0000

    Add create functionality for Traveller profile.

--- travellerprofile/urls.py
+++ travellerprofile/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('travellers/', views.TravellerList, name='travellerlist'),
+    path('travellerprofile/create_traveller/', views.create_traveller, name='create_traveller'),
     path('travellerprofile/view_profile/', views.view_profile, name='view_traveller'),
 ]
--- travellerprofile/views.py
+++ travellerprofile/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.http import HttpResponseRedirect
 from .models import Traveller
 from .forms import TravellerForm
 
@@ -60,3 +61,32 @@ def view_profile(request):
             "traveller": traveller,
         },
     )
+
+def create_traveller(request):
+    """
+    Display form to allow user creation of a .models:Traveller instance
+    """
+    context = {}
+    if request.method == "POST":
+        form = TravellerForm(request.POST, request.FILES)
+        if form.is_valid():
+            firstname = form.cleaned_data.get("firstname")
+            lastname = form.cleaned_data.get("lastname")
+            bio = form.cleaned_data.get("bio")
+            img = form.cleaned_data.get("profile_photo")
+            obj = Traveller.objects.create(
+                                 user = request.user,
+                                 firstname = firstname, 
+                                 lastname = lastname, 
+                                 bio = bio, 
+                                 profile_photo = img
+                                 )
+            obj.save()
+            return HttpResponseRedirect(reverse(view_profile))
+    else:
+        form = TravellerForm()
+    context['form'] = form
+    return render(
+        request,
+        "travellerprofile/create_traveller.html",
+        context,)
