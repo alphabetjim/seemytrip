44cd3ec7f079660671a35ba30b2660f54a5bcd35
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-13 14:02:04 +0000

    Create TravellerForm.

--- travellerprofile/views.py
+++ travellerprofile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Traveller
+from .forms import TravellerForm
+
 
 # Create your views here.
 def homepage(request):
