0e1507817ca9852e157109151ea6c9b1e9edd2e6
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-14 11:46:38 +0000

    Add triplist page

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Trip
 
 # Create your views here.
+def view_trips(request):
+    """
+    Display all trips that have been planned
+    """
+    queryset = Trip.objects.all()
+    
+    return render(
+        request,
+        'trips/triplist.html',
+        {
+            'queryset': queryset,
+        }
+    )
