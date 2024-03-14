dbf21efa0f3e642c20c6b9cbcfc68f99f39da327
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-14 12:16:39 +0000

    Prior to adding custom template tags for list indexing

--- trips/models.py
+++ trips/models.py
@@ -5,17 +5,17 @@ import cloudinary
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-REGIONS = ((0, "UK",), (1, "Europe"),
-     (2, "Asia"), (3, "Oceania"),
-      (4, "North America"), (5, "South America"),
-       (6, "Africa"), (7, "Middle East"))
-ACCOMM_OPTIONS = ((0, "Tent"), (1, "Caravan"), (2, "Camper Van"),
-    (3, "Hotel/Booked Room"))
-
 class Trip(models.Model):
     """
     Stores an individual trip, related to Traveller model
     """
+    REGIONS = ((0, "UK",), (1, "Europe"),
+     (2, "Asia"), (3, "Oceania"),
+      (4, "North America"), (5, "South America"),
+       (6, "Africa"), (7, "Middle East"))
+    ACCOMM_OPTIONS = ((0, "Tent"), (1, "Caravan"), (2, "Camper Van"),
+    (3, "Hotel/Booked Room"))
+
     name = models.CharField(max_length=100, unique=True)
     planner = models.ForeignKey(Traveller, on_delete=models.CASCADE, related_name="trips_planned")
     itinerary = models.TextField(max_length=1000)
@@ -30,4 +30,4 @@ class Trip(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.name} in {self.region} planned by {self.planner}"
+        return f"{self.pk}: {self.name} in {self.region} planned by {self.planner.user}"
--- trips/views.py
+++ trips/views.py
@@ -7,11 +7,18 @@ def view_trips(request):
     Display all trips that have been planned
     """
     queryset = Trip.objects.all()
-    
+    accomm_types = []
+    for trip in queryset:
+        if trip.accomm_type is not None:
+            accomm_types.append(Trip.ACCOMM_OPTIONS[trip.accomm_type][1])
+        else:
+            accomm_types.append("")
+    print(accomm_types)
     return render(
         request,
         'trips/triplist.html',
         {
             'queryset': queryset,
+            'accomm_types': accomm_types,
         }
     )
