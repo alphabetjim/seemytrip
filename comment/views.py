b8e858897c8f6c219a465f86ea1d6452be8f37ce
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-15 09:58:37 +0000

    Add TripComment list view

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import TripComment
 
 # Create your views here.
+class TripCommentList(generic.ListView):
+    """
+    Display all TripComments as a list
+    """ 
+    queryset = TripComment.objects.all()
+    template_name = "comment/commentlist.html"
+    paginate_by = 5
