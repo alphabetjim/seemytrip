076c681435adf45095b30561a46af5ebf4d9f526
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-15 12:31:16 +0000

    Fix bug 05.

--- comment/urls.py
+++ comment/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TripCommentList, name='tripcommentlist'),
-    path('edit_tripcomment/<int:pk>', views.comment_edit, name='edit_tripcomment'),
+    # path('edit_tripcomment/<int:pk>', views.comment_edit, name='edit_tripcomment'),
     # path('delete_tripcomment/<int:comment_id>', views.comment_delete, name='delete_tripcomment'),
 ]
